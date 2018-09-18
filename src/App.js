7afc4e86f3d8b3d79f32dbe7b3c50b37906c0e46
Andrew Vogel <andrewpvogel@gmail.com>
2018-09-18 16:12:11 -0400

    Fetch records from the first page of base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import table from './table';
 
 class App extends Component {
+  state = {
+    result: null,
+    error: null,
+  };
+  componentDidMount() {
+    table('Backlog')
+      .select()
+      .firstPage((error, records) => {
+        if (error) return this.setState({ error });
+        const result = records.map(record => {
+          return {
+            storyName: record.get('Story Name'),
+            priority: record.get('Priority'),
+            useCase: record.get('Use Case'),
+            sprintReleaseDate: record.get('Sprint Release Date'),
+            status: record.get('Status'),
+          };
+        });
+        this.setState({ result });
+      });
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +35,35 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div>
+          <h2>Backlog</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Story Name</th>
+                <th>Status</th>
+                <th>Priority</th>
+                <th>Use Case</th>
+                <th>Sprint Release Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.result &&
+                this.state.result.map((record, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{record.storyName}</td>
+                      <td>{record.status}</td>
+                      <td>{record.priority}</td>
+                      <td>{record.useCase}</td>
+                      <td>{record.sprintReleaseDate}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
